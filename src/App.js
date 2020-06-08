5052bd30d170c9315ed54d10de8aed6e34736291
wajid2990 <wajid2990@gmail.com>
2020-06-08 21:01:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,16 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Finally created first react app
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+          Trying to Learn React
+        
       </header>
     </div>
   );
